241ea704e76dbc0323d57c7e831961257f2604d0
Luis  Senior <heybluguy@yahoo.com>
2018-09-20 00:26:21 -0500

    Deletes tmp images saved.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -39,6 +39,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [800, 600]
   end
 
+  version :small_thumb, from_version: :thumb do
+    process resize_to_fill: [20, 20]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
