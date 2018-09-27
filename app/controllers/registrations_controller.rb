da85feacfcd73513513d89fad5f3ea03cf8e41c3
Luis  Senior <heybluguy@yahoo.com>
2018-09-27 15:54:18 -0500

    fixed typos in private methods

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,12 +2,12 @@ class RegistrationsController < Devise::RegistrationsController
 
 private
 
-  def sign_up_parmas
-    parmas.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    parmas.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
 
 end
